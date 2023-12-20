b968436c725ecf31a4efeb3c424894552674ef15
Nayan Bagale <nvbagale@gmail.com>
2023-12-20 20:43:19 +0530

    git push

--- src/components/sections/video-gallery/Video_Gallery.tsx
+++ src/components/sections/video-gallery/Video_Gallery.tsx
@@ -1,7 +1,23 @@
+'use client';
+
 import { Container } from '@src/components/shared/container';
-import React from 'react';
+import Image from 'next/image';
+import farmers from '@public/images/farmers-image.png';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+
+// import required modules
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
 
 const Video_Gallery = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
     <Container className="flex h-full w-full flex-col items-center justify-center">
       <h1>Video Gallery</h1>
